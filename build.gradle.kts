153758fd2d02dd0cc89b2174608d83b4cffdff40
Vadim Kolchanov <v.kolchanov@headsandhands.ru>
2025-12-13 13:04:11 +0700

    Update Gradle configuration and dependencies for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.0"
     id("co.uzzu.dotenv.gradle") version "4.0.0"
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
 group = "dev.kolchanov"
@@ -13,8 +12,6 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
-    withJavadocJar()
-    withSourcesJar()
 }
 
 repositories {
@@ -24,14 +21,14 @@ repositories {
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
 
     // Jackson Kotlin Support
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
 
     // Web3
     implementation("org.web3j:core:4.14.0")
@@ -40,9 +37,8 @@ dependencies {
     implementation("org.java-websocket:Java-WebSocket:1.5.4")
 
     // HTTP Client
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:5.3.0"))
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.0")
 
     // Logging
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
@@ -63,52 +59,40 @@ tasks.withType<Test> {
     environment = env.allVariables()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            pom {
-                name.set("Polymarket Client")
-                description.set("A Kotlin client for interacting with the Polymarket prediction market platform.")
-                url.set("https://github.com/Vadim-Kolchanov/polymarket-client")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Vadim-Kolchanov")
-                        name.set("Vadim Kolchanov")
-                        email.set("kolchanov.offer@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Vadim-Kolchanov/polymarket-client.git")
-                    developerConnection.set("scm:git:ssh://github.com/Vadim-Kolchanov/polymarket-client.git")
-                    url.set("https://github.com/Vadim-Kolchanov/polymarket-client")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
-                password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
-            }
-        }
-    }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 }
 
-signing {
-    sign(publishing.publications["mavenJava"])
+mavenPublishing {
+    coordinates(group.toString(), name.toString(), version.toString())
+
+    pom {
+        name.set("Polymarket Client")
+        description.set("A Kotlin client for interacting with the Polymarket prediction market platform.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Vadim-Kolchanov/polymarket-client")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("Vadim-Kolchanov")
+                name.set("Vadim Kolchanov")
+                url.set("https://github.com/Vadim-Kolchanov/")
+                email.set("kolchanov.offer@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/Vadim-Kolchanov/polymarket-client.git")
+            developerConnection.set("scm:git:ssh://github.com/Vadim-Kolchanov/polymarket-client.git")
+            url.set("https://github.com/Vadim-Kolchanov/polymarket-client/")
+        }
+    }
 }
